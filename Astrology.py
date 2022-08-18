ca5ae691ec73928f1c8c3fdb0319a39279d51d55
Ddhurvey <devendradhur85@gmail.com>
2022-08-18 18:28:56 +0530

    Asrology

---
+++ Astrology.py
@@ -0,0 +1,32 @@
+import calendar
+##$ Input the data $##
+date=int(input("Please input the birth date. "  ))
+month=str(input("Please input the birth month. "  ))
+##$ Calculate $##
+
+if month=="march" and date >=21 or month=="april" and date<=19:
+    print("Aries('मेष')")
+elif month=="april" and date >=20 or month=="may" and date<=20:
+    print("Taurus('वृष')")
+elif month=="may" and date >=21 or month=="june" and date<=21:
+    print("Gemini('मिथुन')")
+elif month=="june" and date >=22 or month=="july" and date<=22:
+    print("Cancer('कर्क')")
+elif month=="july" and date >=23 or month=="august" and date<=22:
+    print("Leo('सिंह')")
+elif month=="august" and date >=23 or month=="september" and date<=22:
+    print("Virgo('कन्या')")
+elif month=="september" and date >=23 or month=="october" and date<=23:
+    print("Libra('तुला')")
+elif month=="october" and date >=24 or month=="november" and date<=21:
+    print("Scorpio('व्रश्चिक')")
+elif month=="november" and date >=22 or month=="december" and date<=21:
+    print("Sagittarius('धनु')")
+elif month=="december" and date >=22 or month=="january" and date<=19:
+    print("Capricorn('मकर')")
+elif month=="january" and date >=20 or month=="february" and date<=18:
+    print("Aquarius('कुम्भ')")
+elif month=="february" and date >=19 or month=="march" and date<=20:
+    print("Pisces('मीन')")
+else:
+    print("You write Wrong month, please check the spelling.")
