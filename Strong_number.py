59858e11222f0211fe4145a34c6211d4ea962bbf
Ddhurvey <devendradhur85@gmail.com>
2022-09-27 18:04:13 +0530

    strong number

---
+++ Strong_number.py
@@ -0,0 +1,149 @@
+
+
+
+
+
+
+# ####strong number
+# value=int(input("Input the value. :-  "))
+# num=value
+# total=0
+# while value!=0:
+#     colect=value%10
+#     if colect==0:
+#         a=1
+#         total=total+a
+#     elif colect==1:
+#         b=1*1
+#         total=total+b
+#     elif colect==2:
+#         c=1*2
+#         total=total+c
+#     elif colect==3:
+#         d=1*2*3
+#         total=total+d
+#     elif colect==4:
+#         e=1*2*3*4
+#         total=total+e
+#     elif colect==5:
+#         f=1*2*3*4*5
+#         total=total+f
+#     elif colect==6:
+#         g=1*2*3*4*5*6
+#         total=total+g
+#     elif colect==7:
+#         h=1*2*3*4*5*6*7
+#         total=total+h
+#     elif colect==8:
+#         i=1*2*3*4*5*6*7*8
+#         total=total+i
+#     elif colect==9:
+#         j=1*2*3*4*5*6*7*8*9
+#         total=total+j
+#     value=value//10
+# if total==num:
+#     print(num," is a strong number because this number value is.",total)
+# else:
+#     print(num," is not a strong number because this number value is.",total)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#### 2 nd strong number code 
+
+
+
+
+
+
+# value=(input("Input the value. :-  "))
+# num=value
+# total=0
+# for i in value:
+#     if i =='0':
+#         a=1
+#         total=total+a
+#     elif i=='1':
+#         a=1*1
+#         total=total+a
+#     elif i=='2':
+#         a=1*2
+#         total=total+a
+#     elif i=='3':
+#         a=1*2*3
+#         total=total+a
+#     elif i=='4':
+#         a=1*2*3*4
+#         total=total+a
+#     elif i=='5':
+#         a=1*2*3*4*5
+#         total=total+a
+#     elif i=='6':
+#         a=1*2*3*4*5*6
+#         total=total+a
+#     elif i=='7':
+#         a=1*2*3*4*5*6*7
+#         total=total+a
+#     elif i=='8':
+#         a=1*2*3*4*5*6*7*8
+#         total=total+a
+#     elif i=='9':
+#         a=1*2*3*4*5*6*7*8*9
+#         total=total+a
+# print(total)
+# if total==int(num):
+#     print(num," is a strong number because this number value is.",total)
+# else:
+#     print(num," is not a strong number because this number value is.",total)
+
+
+
+
+
