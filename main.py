ed6d3e55aa614c292cf0721a04d8a4a26b60aac3
Ddhurvey <110036026+Ddhurvey@users.noreply.github.com>
2022-10-18 05:42:32 -0700

    Add files via upload

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
 print("Hello World")
+
+
+my_list = []
+
+# ✅ Ignore any exception
+try:
+    print(my_list[100])
+except:
+    # 👇️ this runs
+    pass
+
+
+# ✅ Ignore only IndexError exceptions
+try:
+    print(my_list[100])
+except IndexError:
+    # 👇️ this runs
+    pass
+
+
+
